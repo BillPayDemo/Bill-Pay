9de313af473e9f08153c49360a85957ddbad85cb
Barbora Cernakova <bcernakova01@gmail.com>
2021-09-30 17:28:05 +0100

    Remove empty fragment

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,11 +4,9 @@ import s from "./Footer.module.css";
 
 export const Footer = () => {
   return (
-    <>
-      <div className={s.footerContainer}>
-        <div>Engineered by Theodo</div>
-        <TheodoLogo />
-      </div>
-    </>
+    <div className={s.footerContainer}>
+      <div>Engineered by Theodo</div>
+      <TheodoLogo />
+    </div>
   );
 };
