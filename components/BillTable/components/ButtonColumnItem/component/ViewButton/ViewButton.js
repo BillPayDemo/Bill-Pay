121ec2d5ecedec78510516c6486c232dd6e1ff4a
Chloe Caron <chloec@theodo.co.uk>
2021-10-01 15:32:51 +0100

    Cleaned up the styling

--- components/BillTable/components/ButtonColumnItem/component/ViewButton/ViewButton.js
+++ components/BillTable/components/ButtonColumnItem/component/ViewButton/ViewButton.js
@@ -4,21 +4,7 @@ import s from "./ViewButton.module.css";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { IconButton } from "@mui/material";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  height: "20%",
-  width: "40%",
-  transform: "translate(-50%, -50%)",
-  p: 4,
-  background: "#FFFFFF",
-  outline: "none",
-  boxShadow:
-    "0px 8px 12px rgba(111, 116, 154, 0.08), 0px 3px 16px rgba(111, 116, 154, 0.08), 0px 5px 7px rgba(111, 116, 154, 0.12)",
-  borderRadius: "8px",
-};
+import { modalStyling, closeButtonStyling } from "./styling";
 
 export const ViewButton = (args) => {
   const [open, setOpen] = React.useState(false);
@@ -35,11 +21,11 @@ export const ViewButton = (args) => {
         onClose={handleClose}
         BackdropProps={{ style: { opacity: 0.8, background: "white" } }}
       >
-        <Box sx={style}>
+        <Box sx={modalStyling}>
           <IconButton
             size="small"
             onClick={handleClose}
-            className={s.crossButton}
+            style={closeButtonStyling}
           >
             <CloseIcon />
           </IconButton>
