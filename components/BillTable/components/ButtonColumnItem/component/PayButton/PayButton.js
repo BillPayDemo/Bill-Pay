802cbf18afa932aa2fc4f53b336d3ef8b3fde1fa
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-01 15:37:01 +0100

    Add modal to PayButton

--- components/BillTable/components/ButtonColumnItem/component/PayButton/PayButton.js
+++ components/BillTable/components/ButtonColumnItem/component/PayButton/PayButton.js
@@ -1,7 +1,26 @@
 import React from "react";
 import { Button } from "@codat/orchard-ui";
 import s from "./PayButton.module.css";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 export const PayButton = () => {
-  return <Button label="Pay" className={s.buttonText} />;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button label="Pay" className={s.buttonText} onClick={handleOpen} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        BackdropProps={{ style: { opacity: 0.3 } }}
+      >
+        <Box></Box>
+      </Modal>
+    </div>
+  );
 };
