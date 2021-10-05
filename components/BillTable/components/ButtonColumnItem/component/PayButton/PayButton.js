6b4a0eb97f402fbd9f1a913e7ab341eb7fdce6b0
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-05 12:56:33 +0100

    Refactor pay modal

--- components/BillTable/components/ButtonColumnItem/component/PayButton/PayButton.js
+++ components/BillTable/components/ButtonColumnItem/component/PayButton/PayButton.js
@@ -1,15 +1,7 @@
 import React from "react";
 import { Button } from "@codat/orchard-ui";
 import s from "./PayButton.module.css";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
-import { Divider, IconButton } from "@mui/material";
-import { modalStyling, closeButtonStyling } from "../../Modal.styling";
-import { CloseIcon } from "@codat/orchard-ui";
-import { DotDataPoint } from "../../../../../DotDataPoint/DotDataPoint";
-import { TitleWithSubHeadings } from "../../../../../TitleWithSubHeadings/TitleWithSubHeadings";
-import { getFormattedAmountDue } from "../../../../BillTable.helpers";
-import { PayModalFields } from "./components/PayModalFields/PayModalFields";
+import { PayModal } from "./components/PayModal/PayModal";
 
 export const PayButton = ({ billData }) => {
   const [isPayModalOpen, setPayModalOpen] = React.useState(false);
@@ -24,53 +16,11 @@ export const PayButton = ({ billData }) => {
           className={s.buttonText}
           onClick={handlePayModalOpen}
         />
-        <Modal
-          open={isPayModalOpen}
-          onClose={handlePayModalClose}
-          BackdropProps={{ style: { opacity: 0.8, background: "white" } }}
-        >
-          <Box sx={modalStyling}>
-            <IconButton
-              size="small"
-              onClick={handlePayModalClose}
-              style={closeButtonStyling}
-            >
-              <CloseIcon />
-            </IconButton>
-            {billData && (
-              <div className={s.topSectionContainer}>
-                <TitleWithSubHeadings
-                  upperTitle={billData.id}
-                  mainTitle="Bill Payment"
-                  lowerTitle={
-                    <DotDataPoint
-                      leftText={billData.supplierName}
-                      rightText={billData.accountName}
-                    />
-                  }
-                  mainTitleCustomFontSize="26px"
-                />
-                <TitleWithSubHeadings
-                  upperTitle="Amount Due"
-                  mainTitle={getFormattedAmountDue(
-                    billData.currency,
-                    billData.amountDue
-                  )}
-                  mainTitleCustomFontSize="20px"
-                />
-              </div>
-            )}
-            <Divider />
-            <PayModalFields />
-            <div className={s.payBillButtonContainer}>
-              <Button
-                label="Pay Bill"
-                className={s.payBillButton}
-                onClick={handlePayModalClose}
-              />
-            </div>
-          </Box>
-        </Modal>
+        <PayModal
+          isPayModalOpen={isPayModalOpen}
+          handlePayModalClose={handlePayModalClose}
+          billData={billData}
+        />
       </td>
     </tr>
   );
---
+++ components/BillTable/components/ButtonColumnItem/component/PayButton/components/PayModal/PayModal.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Button } from "@codat/orchard-ui";
+import s from "./PayModal.module.css";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { Divider, IconButton } from "@mui/material";
+import { modalStyling, closeButtonStyling } from "../../../../Modal.styling";
+import { CloseIcon } from "@codat/orchard-ui";
+import { DotDataPoint } from "../../../../../../../DotDataPoint/DotDataPoint";
+import { TitleWithSubHeadings } from "../../../../../../../TitleWithSubHeadings/TitleWithSubHeadings";
+import { getFormattedAmountDue } from "../../../../../../BillTable.helpers";
+import { PayModalFields } from "../PayModalFields/PayModalFields";
+
+export const PayModal = ({ isPayModalOpen, handlePayModalClose, billData }) => {
+  return (
+    <Modal
+      open={isPayModalOpen}
+      onClose={handlePayModalClose}
+      BackdropProps={{ style: { opacity: 0.8, background: "white" } }}
+    >
+      <Box sx={modalStyling}>
+        <IconButton
+          size="small"
+          onClick={handlePayModalClose}
+          style={closeButtonStyling}
+        >
+          <CloseIcon />
+        </IconButton>
+        {billData && (
+          <div className={s.topSectionContainer}>
+            <TitleWithSubHeadings
+              upperTitle={billData.id}
+              mainTitle="Bill Payment"
+              lowerTitle={
+                <DotDataPoint
+                  leftText={billData.supplierName}
+                  rightText={billData.accountName}
+                />
+              }
+              mainTitleCustomFontSize="26px"
+            />
+            <TitleWithSubHeadings
+              upperTitle="Amount Due"
+              mainTitle={getFormattedAmountDue(
+                billData.currency,
+                billData.amountDue
+              )}
+              mainTitleCustomFontSize="20px"
+            />
+          </div>
+        )}
+        <Divider />
+        <PayModalFields />
+        <div className={s.payBillButtonContainer}>
+          <Button
+            label="Pay Bill"
+            className={s.payBillButton}
+            onClick={handlePayModalClose}
+          />
+        </div>
+      </Box>
+    </Modal>
+  );
+};
