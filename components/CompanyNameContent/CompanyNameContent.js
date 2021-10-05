c04a5732c04676178fbe1464fae4b5752b7cba1a
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-05 08:38:11 +0100

    Allow text field to be interactive

--- components/CompanyNameContent/CompanyNameContent.js
+++ components/CompanyNameContent/CompanyNameContent.js
@@ -4,13 +4,16 @@ import "../../node_modules/@codat/orchard-ui/dist/index.css";
 import s from "./CompanyNameContent.module.css";
 
 export const CompanyNameContent = () => {
+  const [companyNameValue, setCompanyNameValue] = React.useState("");
+
   return (
     <div className={s.inputContainer}>
       <TextInput
         id="company-name"
         label="Enter your company name"
         placeholder="e.g. Pete's Pies"
-        value=""
+        value={companyNameValue}
+        onChange={(event) => setCompanyNameValue(event.target.value)}
       />
       <Button label="Next" className={s.button} onClick={() => null} />
     </div>
