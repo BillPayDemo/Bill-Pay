9b4f21fec5940dc1bfe27f558909bb90c0b3d572
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-07 11:38:46 +0100

    Refactor styling into separate file

--- pages/company-name.js
+++ pages/company-name.js
@@ -1,39 +1,16 @@
 import { Footer } from "../components/Footer/Footer";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import { CompanyNameContent } from "../components/CompanyNameContent/CompanyNameContent";
 import Box from "@mui/material/Box";
-
-const modalStyling = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  width: "50%",
-  height: "30%",
-  transform: "translate(-50%, -80%)",
-  p: 4,
-  background: "#FFFFFF",
-  outline: "none",
-  boxShadow:
-    "0px 8px 12px rgba(111, 116, 154, 0.08), 0px 3px 16px rgba(111, 116, 154, 0.08), 0px 5px 7px rgba(111, 116, 154, 0.12)",
-  borderRadius: "8px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  // margin: 0,
-  padding: 0,
-};
+import { companyNameStyling } from "../styles/CompanyName.styling";
 
 export default function CompanyName() {
   return (
     <>
-      <div style={{ backgroundColor: "red!important" }}>
-        <Box sx={modalStyling}>
-          {/* <div className={styles.container}> */}
-          <CompanyNameContent />
-          {/* </div> */}
-        </Box>
-      </div>
+      <style>{"body { background-color: #F2F2F2; }"}</style>
+      <Box sx={companyNameStyling}>
+        <CompanyNameContent />
+      </Box>
       <h4
         style={{
           display: "flex",
