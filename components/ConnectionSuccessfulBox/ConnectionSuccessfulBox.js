ae3133cbc6cbabc4a31c8237c62bb67ec98472cb
Chloe Caron <chloec@theodo.co.uk>
2021-10-08 13:32:36 +0100

    Session storage check implemented

--- components/ConnectionSuccessfulBox/ConnectionSuccessfulBox.js
+++ components/ConnectionSuccessfulBox/ConnectionSuccessfulBox.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LeftHandPanel } from "./components/LeftHandPanel/LeftHandPanel";
 import { RightHandPanel } from "./components/RightHandPanel/RightHandPanel";
 import s from "./ConnectionSuccessfulBox.module.css";
 
 export const ConnectionSuccessfulBox = () => {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    setState(window.sessionStorage.getItem("companyId"));
+  }, [setState]);
+
+  console.log(state);
   return (
     <div className={s.box}>
       <LeftHandPanel />
