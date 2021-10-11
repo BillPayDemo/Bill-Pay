0cffe5bf07d7ba0ae31156027da9e688d92005d0
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-11 17:26:31 +0100

    Add a loading page

--- pages/bills.js
+++ pages/bills.js
@@ -7,6 +7,10 @@ import Divider from "@mui/material/Divider";
 import { Footer } from "../components/Footer/Footer";
 import Head from "next/head";
 import { useState, useEffect } from "react";
+import { Spinner, Typography } from "@codat/orchard-ui";
+import s from "../styles/Bills.module.css";
+import Box from "@mui/material/Box";
+import { boxStyling } from "../styles/Bills.styling";
 
 const fetcher = (url) =>
   axios.get(url).then((res) => {
@@ -25,6 +29,12 @@ const fetcherWithId = (url, companyId) =>
     });
 
 export default function Bills() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
+
   const [companyId, setValue] = useState("");
 
   useEffect(() => {
@@ -81,32 +91,51 @@ export default function Bills() {
     }));
 
   return (
-    <div>
-      <Head>
-        <title>Bills</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <CompanyHeader />
-      <div
-        style={{ paddingTop: "48px", paddingRight: "15%", paddingLeft: "15%" }}
-      >
-        {dataCompanyInfo !== undefined && (
-          <div style={{ marginBottom: "25px" }}>
-            <TitleWithSubHeadings
-              mainTitle="Bill Pay"
-              upperTitle={dataCompanyInfo.companyName}
-              lowerTitle="Easily view and pay outstanding supplier invoices"
+    <>
+      {loading === false ? (
+        <div>
+          <Head>
+            <title>Bills</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <CompanyHeader />
+          <div
+            style={{
+              paddingTop: "48px",
+              paddingRight: "15%",
+              paddingLeft: "15%",
+            }}
+          >
+            {dataCompanyInfo !== undefined && (
+              <div style={{ marginBottom: "25px" }}>
+                <TitleWithSubHeadings
+                  mainTitle="Bill Pay"
+                  upperTitle={dataCompanyInfo.companyName}
+                  lowerTitle="Easily view and pay outstanding supplier invoices"
+                />
+              </div>
+            )}
+            <Divider />
+            <BillTable
+              billData={listBills}
+              accountData={accounts}
+              billStatus={billStatus}
             />
+            <Footer />
           </div>
-        )}
-        <Divider />
-        <BillTable
-          billData={listBills}
-          accountData={accounts}
-          billStatus={billStatus}
-        />
-        <Footer />
-      </div>
-    </div>
+        </div>
+      ) : (
+        <>
+          <Box sx={boxStyling} className={s.loadingPage}>
+            <style>{"body { background-color: #f7f8ff; }"}</style>
+            <div className={s.spinner}>
+              <Typography variant="h2">Finding your bills...</Typography>
+              <Spinner />
+            </div>
+          </Box>
+          <Footer />
+        </>
+      )}
+    </>
   );
 }
---
+++ styles/Bills.styling.js
@@ -0,0 +1,14 @@
+export const boxStyling = {
+  position: "absolute",
+  top: "45%",
+  left: "50%",
+  width: "50%",
+  transform: "translate(-50%, -50%)",
+  p: 4,
+  background: "#FFFFFF",
+  outline: "none",
+  padding: "0",
+  boxShadow:
+    "0px 8px 12px rgba(111, 116, 154, 0.08), 0px 3px 16px rgba(111, 116, 154, 0.08), 0px 5px 7px rgba(111, 116, 154, 0.12)",
+  borderRadius: "8px",
+};
