166d874080c2ba5fd1e72e163827c2be171bf4ca
Barbora Cernakova <bcernakova01@gmail.com>
2021-10-11 15:49:47 +0100

    Remove unused import

--- components/BillTable/components/ButtonColumnItem/ButtonColumnItem.js
+++ components/BillTable/components/ButtonColumnItem/ButtonColumnItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import "../../../../node_modules/@codat/orchard-ui/dist/index.css";
 import s from "./ButtonColumnItem.module.css";
 import { PayButton } from "./component/PayButton/PayButton";
