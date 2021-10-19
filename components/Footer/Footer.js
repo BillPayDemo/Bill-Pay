458ffa3a8cfac9021d9d9794d3194df77d326889
Chloe <cmlc3@cam.ac.uk>
2021-10-19 09:37:14 +0100

    Changed font on windows computer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { TheodoLogo } from "../TheodoLogo/TheodoLogo";
 import s from "./Footer.module.css";
+import { Typography } from "@codat/orchard-ui";
 
 export const Footer = () => {
   return (
     <div className={s.footerContainer}>
-      <div>Engineered by Theodo</div>
+      <Typography
+        style={{
+          margin: 0,
+          color: "#ffffff",
+          fontSize: "16px",
+          fontWeight: "400",
+        }}
+        variant="small"
+      >
+        Engineered by Theodo
+      </Typography>
       <TheodoLogo />
     </div>
   );
