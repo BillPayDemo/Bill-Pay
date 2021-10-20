3d978013dec084374dbcfdb9193bafb050c14b1e
Chloe Caron <chloec@theodo.co.uk>
2021-10-20 12:35:47 +0100

    Added logout button to clear session storage and redirect to first page

--- components/CompanyHeader/CompanyHeader.js
+++ components/CompanyHeader/CompanyHeader.js
@@ -1,20 +1,28 @@
 import React from "react";
 import s from "./CompanyHeader.module.css";
-import { BankIcon, Typography } from "@codat/orchard-ui";
+import { BankIcon, Typography, Button } from "@codat/orchard-ui";
 
 export const CompanyHeader = () => {
+  const handleLogout = () => {
+    sessionStorage.clear();
+    window.location.href = "/";
+  };
+
   return (
-    <div className={s.header}>
-      <BankIcon fillColor="#482DEB" className={s.icon} />
-      <Typography
-        style={{
-          color: "#482DEB",
-          marginBottom: "0px",
-        }}
-        variant="h3"
-      >
-        CoBank
-      </Typography>
+    <div className={s.container}>
+      <div className={s.header}>
+        <BankIcon fillColor="#482DEB" className={s.icon} />
+        <Typography
+          style={{
+            color: "#482DEB",
+            marginBottom: "0px",
+          }}
+          variant="h3"
+        >
+          CoBank
+        </Typography>
+      </div>
+      <Button label="Logout" onClick={() => handleLogout()} />
     </div>
   );
 };
